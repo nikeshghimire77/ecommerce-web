5d3c529ce5d7d49d1817b3e6144c9374b71c99a9
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-12-24 07:33:03 -0600

    Added evaluator to derive the expression

---
+++ backend/Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        //public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpeficication<TEntity> spec)
+        //{
+
+        //}
+
+    }
+}
