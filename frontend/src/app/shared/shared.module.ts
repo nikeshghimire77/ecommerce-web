aa029ce19000d661ca1fbcc1d622ba84b452a21f
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-12-29 15:46:37 -0600

    Strutured with pagination, filtering + refactored

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  PaginationModule } from 'ngx-bootstrap/pagination';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    PaginationModule.forRoot()
-  ],
-  exports: [PaginationModule]
+  declarations: [PagingHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot()],
+  exports: [PaginationModule, PagingHeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
--- frontend/src/app/shop/shop.service.ts
+++ frontend/src/app/shop/shop.service.ts
@@ -7,12 +7,12 @@ import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
@@ -23,19 +23,21 @@ export class ShopService {
 
     if (shopParams.typeId !== 0) {
       params = params.append('typeId', shopParams.typeId.toString());
-    } 
+    }
     params = params.append('sort', shopParams.sort);
     params = params.append('pageIndex', shopParams.pageNumber.toString());
     params = params.append('pageSize', shopParams.pageSize.toString());
 
- 
-
-    return this.http.get<IPagination>(this.baseUrl + 'products',{observe: 'response', params})
-    .pipe(
-      map(response => {
-        return response.body;
+    return this.http
+      .get<IPagination>(this.baseUrl + 'products', {
+        observe: 'response',
+        params,
       })
-    );
+      .pipe(
+        map((response) => {
+          return response.body;
+        })
+      );
   }
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
