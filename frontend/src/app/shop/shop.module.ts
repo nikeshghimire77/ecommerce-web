495e20f379a613728a35ce2136baaa4ee39468a0
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-12-31 10:02:40 -0600

    Made use of lazy loading to prevent all routes

--- frontend/src/app/shop/shop.module.ts
+++ frontend/src/app/shop/shop.module.ts
@@ -4,11 +4,10 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
+import { ShopRoutineModule } from './shop-routine.module';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
-  exports: [ShopComponent, ProductItemComponent],
+  imports: [CommonModule, SharedModule, ShopRoutineModule],
 })
 export class ShopModule {}
